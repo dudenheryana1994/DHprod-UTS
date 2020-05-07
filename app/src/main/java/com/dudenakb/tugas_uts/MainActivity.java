4dc89d055394dbe2df23c9e6c82c7a5dbd25efee
dudenheryana1994 <heryana.duden@gmail.com>
2020-05-07 14:24:14 +0700

    update progress 07-April-2020

---
+++ app/src/main/java/com/dudenakb/tugas_uts/MainActivity.java
@@ -0,0 +1,38 @@
+package com.dudenakb.tugas_uts;
+
+//Tanggal Pengerjaaan : 07 Mei 2020
+//Deskripsi Pengerjaan :Membuat Splash Screen dan Walkthrough
+// NIM : 10117137
+// Nama : Duden Heryana
+// Kelas : IF-4
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private int waktu_loading=4000;
+
+    //4000=4 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent intro1=new Intent(MainActivity.this, intro.class);
+                startActivity(intro1);
+                finish();
+            }
+        },waktu_loading);
+
+    }
+}
---
+++ app/src/main/java/com/dudenakb/tugas_uts/intro1.java
@@ -0,0 +1,20 @@
+package com.dudenakb.tugas_uts;
+
+//Tanggal Pengerjaaan : 07 Mei 2020
+//Deskripsi Pengerjaan :Membuat Splash Screen dan Walkthrough
+// NIM : 10117137
+// Nama : Duden Heryana
+// Kelas : IF-4
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class intro1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro1);
+    }
+}
