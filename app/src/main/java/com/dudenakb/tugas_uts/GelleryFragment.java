9660b781ed80a37dfd41d803be2975833adf5d6f
dudenheryana1994 <heryana.duden@gmail.com>
2020-05-15 16:35:56 +0700

    Update Progress 15 Mei 2020

--- app/src/main/java/com/dudenakb/tugas_uts/GelleryFragment.java
+++ app/src/main/java/com/dudenakb/tugas_uts/GelleryFragment.java
@@ -23,9 +23,16 @@ import androidx.fragment.app.Fragment;
 
 public class GelleryFragment extends Fragment {
 
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_gallery, container, false);
+
+
+
+
+
     }
 }
